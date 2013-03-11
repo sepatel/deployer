341086ede9993ddc081505abe58cccee2d952399
Sejal Patel <sejal@inigma.org>
2013-03-10 23:02:40 -0400

    Functional Proof of Concept Completed
    Can take a war file from a url, process it using the dot templating
    system, invoke pre/post deploy scripts, and deploy to the local
    filesystem. deploy to http and other protocol types is not yet
    supported however.
    
    Squashed commit of the following:
    
    commit eb51c233aac5b63a7475ffa73f73fe10d84251fc
    Author: Sejal Patel <sejal@inigma.org>
    Date:   Sun Mar 10 23:01:49 2013 -0400
    
        Fully functional happy path.
    
        Implemented a local filesystem based deploy along with pre/post deploy
        script executions.
    
    commit b7f119beb712652dca3041a6e052120778608cf1
    Author: Sejal Patel <sejal@inigma.org>
    Date:   Sun Mar 10 15:32:51 2013 -0400
    
        Improved to download resource and process template
    
        This does the grunt of the work of retrieving a resource, going through
        each file within the resource looking for template resources, and then
        processing each template with the given configurations within a new
        resource that will later be used for "deployment".

--- src/main/java/DotConfigure.java
+++ src/main/java/DotConfigure.java
@@ -3,13 +3,30 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import javax.script.SimpleBindings;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.logging.Logger;
 
 /**
  * @author <a href='mailto:sejal@sejal.org">Sejal Patel</a>
@@ -17,11 +34,13 @@ import java.util.Scanner;
  */
 public class DotConfigure {
     private static ScriptEngine JAVASCRIPT = new ScriptEngineManager().getEngineByName("JavaScript");
+    private static Logger logger = Logger.getLogger(DotConfigure.class.getName());
 
     static { // initialize the javascript engine with the doT.js implementation
         InputStream in = DotConfigure.class.getResourceAsStream("doT.js");
         try {
             JAVASCRIPT.eval(new InputStreamReader(in));
+            JAVASCRIPT.eval("doT.templateSettings.strip = false");
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
@@ -32,14 +51,155 @@ public class DotConfigure {
     public DotConfigure(String configUrl) {
         if (configUrl == null) {
             throw new DotException("URL to configuration is required!");
-        } else if (!configUrl.matches("^\\w+:.*")) {
-            configUrl = "file:" + configUrl;
         }
         try {
-            URL url = new URL(configUrl);
-            Scanner scanner = new Scanner(url.openStream()).useDelimiter("\\A"); // beginning of input boundary
+            URL url = getUrl(configUrl);
+            initialize(url.openStream());
+        } catch (IOException e) {
+            throw new DotException("Unable to open resource located at " + configUrl, e);
+        }
+    }
+
+    public DotConfigure(InputStream config) {
+        initialize(config);
+    }
+
+    public DotConfiguration getConfig() {
+        return config;
+    }
+
+    public void invoke() {
+        File resource = downloadResource();
+        File processedResource = null;
+        if ("war".equalsIgnoreCase(config.getType())) {
+            try {
+                processedResource = processWar(new JarFile(resource));
+            } catch (IOException e) {
+                throw new DotException("Resource not a war: " + resource, e);
+            }
+        } else {
+            logger.warning("Terminating, unknown handling of type " + config.getType());
+            return;
+        }
+
+        executeScripts(config.getPreDeploy());
+
+        deploy(processedResource);
+
+        executeScripts(config.getPostDeploy());
+    }
+
+    private void copy(InputStream in, OutputStream out) {
+        byte[] buffer = new byte[4096];
+        int read = -1;
+        try {
+            while ((read = in.read(buffer)) > 0) {
+                out.write(buffer, 0, read);
+            }
+        } catch (IOException e) {
+            throw new DotException(e);
+        }
+    }
+
+    private void deploy(File resource) {
+        try {
+            URL url = getUrl(config.getDeploy());
+            if ("file".equals(url.getProtocol())) {
+                FileOutputStream fos = new FileOutputStream(url.getPath());
+                FileInputStream fis = new FileInputStream(resource);
+                copy(fis, fos);
+                fis.close();
+                fos.close();
+            } else if ("http".equals(url.getProtocol()) || "https".equals(url.getProtocol())) {
+
+            } else if ("ftp".equals(url.getProtocol())) {
+
+            } else if ("scp".equals(url.getProtocol())) {
+
+            }
+        } catch (IOException e) {
+            throw new DotException(e);
+        }
+    }
+
+    private File downloadResource() {
+        if (!config.getUrl().matches("^\\w+:.*")) {
+            return new File(config.getUrl());
+        }
+
+        FileOutputStream fos = null;
+        try {
+            File temp = File.createTempFile("", "." + config.getType());
+            fos = new FileOutputStream(temp);
+            URL url = getUrl(config.getUrl());
+            copy(url.openStream(), fos);
+            return temp;
+        } catch (IOException e) {
+            throw new DotException(e);
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    logger.warning("Integrity issue with downloaded resource: " + config.getUrl());
+                }
+            }
+        }
+    }
+
+    private void executeScripts(List<String> scripts) {
+        for (String script : scripts) {
+            File exec = null;
+            FileOutputStream fos = null;
+            try {
+                exec = File.createTempFile("script", ".sh");
+                exec.setExecutable(true);
+                fos = new FileOutputStream(exec);
+                fos.write(script.getBytes());
+                fos.close();
+
+                Process process = Runtime.getRuntime().exec(exec.getAbsolutePath());
+                process.waitFor();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                copy(process.getInputStream(), baos);
+                copy(process.getErrorStream(), baos);
+                if (process.exitValue() != 0) {
+                    logger.severe(baos.toString());
+                    throw new DotException("Exit status " + process.exitValue() + " executing:\n" + script);
+                } else {
+                    logger.info(baos.toString());
+                }
+            } catch (IOException e) {
+                throw new DotException(e.getMessage() + " executing:\n" + script);
+            } catch (InterruptedException e) {
+                throw new DotException(e.getMessage() + " executing:\n" + script);
+            } finally {
+                if (exec != null) {
+                    exec.delete();
+                }
+                if (fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        logger.warning("Error creating shell execution: " + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    private URL getUrl(String url) throws MalformedURLException {
+        if (!url.matches("^\\w+:.*")) {
+            return new URL("file:" + url);
+        }
+        return new URL(url);
+    }
+
+    private void initialize(InputStream in) {
+        try {
+            Scanner scanner = new Scanner(in).useDelimiter("\\A"); // beginning of input boundary
             if (!scanner.hasNext()) {
-                throw new DotException("Config URL: " + configUrl + " contains no data");
+                throw new DotException("Configuration contains no data");
             }
             SimpleBindings bindings = new SimpleBindings();
             bindings.putAll(JAVASCRIPT.getBindings(ScriptContext.ENGINE_SCOPE));
@@ -54,15 +214,65 @@ public class DotConfigure {
             config.setPreDeploy((List<String>) json.get("preDeploy"));
             config.setPostDeploy((List<String>) json.get("postDeploy"));
             config.setTemplateExtension((String) json.get("templateExtension"));
-        } catch (IOException e) {
-            throw new DotException(e);
         } catch (ScriptException e) {
             throw new DotException(e);
         }
     }
 
-    public DotConfiguration getConfig() {
-        return config;
+    private InputStream processTemplate(InputStream in) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        copy(in, baos);
+        SimpleBindings bindings = new SimpleBindings();
+        bindings.putAll(JAVASCRIPT.getBindings(ScriptContext.ENGINE_SCOPE));
+        bindings.put("template", baos.toString());
+        bindings.put("config", config.getConfig());
+        try {
+            String result = (String) JAVASCRIPT.eval("doT.template(template)(config)", bindings);
+            return new ByteArrayInputStream(result.getBytes());
+        } catch (ScriptException e) {
+            throw new DotException("Unable to evaluate template", e);
+        }
+    }
+
+    private File processWar(JarFile jarFile) {
+        String ext = config.getTemplateExtension();
+        Enumeration<JarEntry> entries = jarFile.entries();
+        Set<String> ignores = new HashSet<String>();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            String name = jarEntry.getName();
+            if (name.endsWith(ext)) {
+                ignores.add(name.substring(0, name.lastIndexOf(ext)));
+            }
+        }
+
+        entries = jarFile.entries();
+        try {
+            File temp = File.createTempFile("dot-", ".war");
+            JarOutputStream jos = new JarOutputStream(new FileOutputStream(temp));
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (ignores.contains(entry.getName())) { // template exists so don't copy over the original
+                    continue;
+                }
+                String name = entry.getName();
+                if (name.endsWith(ext)) {
+                    JarEntry newentry = new JarEntry(name.substring(0, name.lastIndexOf(ext)));
+                    newentry.setTime(entry.getTime());
+                    jos.putNextEntry(newentry);
+                    copy(processTemplate(jarFile.getInputStream(entry)), jos);
+                    jos.closeEntry();
+                } else {
+                    jos.putNextEntry(entry);
+                    copy(jarFile.getInputStream(entry), jos);
+                    jos.closeEntry();
+                }
+            }
+            jos.close();
+            return temp;
+        } catch (IOException e) {
+            throw new DotException(e);
+        }
     }
 }
 
--- src/test/java/DotConfigureTest.java
+++ src/test/java/DotConfigureTest.java
@@ -1,5 +1,3 @@
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import javax.script.ScriptException;
@@ -10,15 +8,21 @@ import static org.junit.Assert.*;
 public class DotConfigureTest {
     @Test
     public void initialize() throws IOException, ScriptException {
-        DotConfigure dot = new DotConfigure("file:src/test/resources/sample-config.json");
+        DotConfigure dot = new DotConfigure("src/test/resources/sample-config.json");
         DotConfiguration config = dot.getConfig();
         assertNotNull(config);
         assertEquals("war", config.getType());
-        assertEquals("target/test-1.0.war", config.getUrl());
+        assertEquals("http://localhost/download/test-1.0.war", config.getUrl());
         assertEquals("http://tomcat:tomcat@localhost:8080/manager/deploy?path=/test&update=true", config.getDeploy());
         assertEquals("Flying Purple Monkey", config.getConfig().get("name"));
         assertEquals(0, config.getPreDeploy().size());
         assertEquals(0, config.getPostDeploy().size());
         assertEquals(".dot", config.getTemplateExtension());
+    }
+
+    @Test
+    public void invoke() {
+        DotConfigure dot = new DotConfigure(DotConfigure.class.getResourceAsStream("/invoke-config.json"));
+        dot.invoke();
     }
 }
